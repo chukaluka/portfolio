8804d268125c5a64b5d867578974ade97745224b
Chukwuka Okonkwo David <chukwukaokonkwo99@gmail.com>
2024-10-19 02:07:54 +0100

    updated about

--- src/components/About.js
+++ src/components/About.js
@@ -11,8 +11,8 @@ export default function About() {
     initial={{opacity: 0, y: 75}}
     whileInView={{opacity: 1, y: 0}}
     transition={{duration: 0.5, delay: 0.25}}
-    className='text-white/70 pl-28 pr-28 pt-20 mt-16'>
-        <div className='text-center font-bold text-6xl md:text-8xl'>
+    className='text-white/70 max-w-[400px] md:max-w-[800px] pt-20 mt-16'>
+        <div className='text-center font-bold text-3xl md:text-8xl'>
             <h1>About Me</h1>
         </div>
 
@@ -29,7 +29,7 @@ export default function About() {
                     className='a-img relative z-10'
                 />
             </div>
-            <div className='flex-1 p-4'>
+            <div className=' flex-1 p-4 '>
                 <h1 className='text-white/70'>
                     A little about me.<br/> I'm just a guy who got into coding because it was cool to write codes and confuse people, I felt like a hacker. Unserious at first but I picked challenge from my close friends and people around me. I became self-taught and interned in different companies. I dabble a little in blockchain technology but I mostly code frontend and mobile apps. I am an AI and Robotics enthusiast, so that could be a future plan. Oh yeah, I also bagged a degree in computer science.<br/> 
                     When I'm not coding, I'm either on the keyboard, watching a movie, playing football or strolling. There's more to these though, but i straight up just take leisure when I'm not coding.
